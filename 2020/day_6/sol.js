ddb7292a4082f352740c9eb0ad40e38025bb0b5b
Ivan Vyshnevskyi <ivan@vyshnevskyi.com>
2020-12-06 07:29:28 +0200

    add solutions for day 6 of 2020

---
+++ 2020/day_6/sol.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+let fs = require("fs");
+
+let input = fs
+	.readFileSync("./input", "utf-8")
+	.split("\n\n")
+	.filter((v) => v)
+	.map((v) => v.split("\n"));
+
+console.log(`read in ${input.length} records...`);
+console.log(input.slice(0, 10));
+
+let m = 0;
+for (let g of input) {
+	let s = new Set();
+	for (let p of g) {
+		for (let c of p.split('')) {
+			s.add(c)
+		}
+	}
+	m += s.size;
+}
+
+console.log(m)
