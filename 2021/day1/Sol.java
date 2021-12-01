5b0e36e531043fd9a5a366b0d690fa2fc46bd078
Ivan Vyshnevskyi <ivan@vyshnevskyi.com>
2021-12-01 07:11:06 +0200

    add solutions for day 1 of 2021

--- 2021/day1/Sol.java
+++ 2021/day1/Sol.java
@@ -10,6 +10,16 @@ import static util.Util.*;
 public class Sol {
 
     static final String test = """
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
             """;
 
     public static void main(String[] args) throws IOException {
@@ -18,9 +28,17 @@ public class Sol {
 //        String[] input = testStrings(1);
 //        String[] input = inputStrings(1);
 //        long[] input = testLongs(1);
-//        long[] input = inputLongs(1);
+        long[] input = inputLongs(1);
 
         long result = 0;
+        long prev = -1;
+        for (int i = 0; i < input.length; i++) {
+            long d = input[i];
+            if (prev != -1) {
+                result += d > prev ? 1 : 0;
+            }
+            prev = d;
+        }
         log("Result: %d", result);
     }
 
--- 2021/day1/Sol2.java
+++ 2021/day1/Sol2.java
@@ -2,7 +2,6 @@ package day1;
 
 import java.io.IOException;
 
-import static day1.Sol.*;
 import static util.Util.*;
 
 public class Sol2 {
@@ -13,9 +12,20 @@ public class Sol2 {
 //        String[] input = testStrings(1);
 //        String[] input = inputStrings(1);
 //        long[] input = testLongs(1);
-//        long[] input = inputLongs(1);
+        long[] input = inputLongs(1);
 
         long result = 0;
+        long[] windows = new long[input.length - 2];
+        for (int i = 0; i < input.length - 2; i++) {
+            windows[i] = input[i] + input[i + 1] + input[i + 2];
+        }
+        long prev = -1;
+        for (int i = 0; i < windows.length; i++) {
+            if (prev != -1) {
+                result += windows[i] > prev ? 1 : 0;
+            }
+            prev = windows[i];
+        }
         log("Result: %d", result);
     }
 }
