843ce78c5066885df3fa1f0a0f8859daaff387e9
Ivan Vyshnevskyi <ivan@vyshnevskyi.com>
2021-12-11 07:26:03 +0200

    add solution for 2021 day 11

---
+++ 2021/src/solutions/Day11.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+
+/*
+      -------Part 1--------   -------Part 2--------
+Day       Time  Rank  Score       Time  Rank  Score
+ 11   00:22:32  1582      0   00:24:33  1423      0
+ */
+
+let utils = require("../utils/utils");
+let {last, longs, remove, swap} = utils;
+
+function main() {
+    let input = utils.day(11);
+    let sample1 = utils.sample(`
+    5483143223
+    2745854711
+    5264556173
+    6141336146
+    6357385478
+    4167524645
+    2176841721
+    6882881134
+    4846848554
+    5283751526
+    `);
+
+    let expected1_1 = 1656;
+    console.log(`Sample: ${part1(sample1)} (expected: ${expected1_1})`);
+    console.log(`Result: ${part1(input)}`);
+
+    console.log("---")
+
+    let expected1_2 = 195;
+    console.log(`Sample: ${part2(sample1)} (expected: ${expected1_2})`);
+    console.log(`Result: ${part2(input)}`);
+}
+
+function neighbors(field, y, x) {
+    let res = [];
+    if (y > 0) {
+        res.push([y - 1, x]);
+        if (x > 0) {
+            res.push([y - 1, x - 1]);
+        }
+        if (x < field[y].length - 1) {
+            res.push([y - 1, x + 1]);
+        }
+    }
+    if (y < field.length - 1) {
+        res.push([y + 1, x]);
+        if (x > 0) {
+            res.push([y + 1, x - 1]);
+        }
+        if (x < field[y].length - 1) {
+            res.push([y + 1, x + 1]);
+        }
+    }
+    if (x > 0) {
+        res.push([y, x - 1]);
+    }
+    if (x < field[y].length - 1) {
+        res.push([y, x + 1]);
+    }
+
+    return res;
+}
+
+function step(field) {
+    let flashed = field.map(v => v.map(s => false));
+    let critical = []
+    for (let y = 0; y < field.length; y++) {
+        for (let x = 0; x < field[y].length; x++) {
+            field[y][x] += 1;
+            if (field[y][x] > 9) {
+                flashed[y][x] = true;
+                critical.push([y, x]);
+            }
+        }
+    }
+    while (critical.length > 0) {
+        let [y, x] = critical.pop();
+        for (let [ny, nx] of neighbors(field, y, x)) {
+            field[ny][nx] += 1;
+            if (!flashed[ny][nx] && field[ny][nx] > 9) {
+                flashed[ny][nx] = true;
+                critical.push([ny, nx]);
+            }
+        }
+    }
+    for (let y = 0; y < field.length; y++) {
+        for (let x = 0; x < field[y].length; x++) {
+            if (flashed[y][x]) {
+                field[y][x] = 0;
+            }
+        }
+    }
+    return flashed.flat().filter(v => v).length;
+}
+
+function part1(strings) {
+    let longs = strings.map(line => line.split('').filter(v => v).map(v => parseInt(v.trim(), 10)));
+
+    let result = 0;
+    for (let i = 0; i < 100; i++) {
+        result += step(longs);
+    }
+
+    return result;
+}
+
+function part2(strings) {
+    let longs = strings.map(line => line.split('').filter(v => v).map(v => parseInt(v.trim(), 10)));
+    let len = longs.map(row => row.length).reduce((a, b) => a + b, 0);
+
+    let result = 0;
+    for (let i = 0; i < 10_000_000; i++) {
+        let count = step(longs);
+        if (count === len) {
+            result = i + 1;
+            break;
+        }
+    }
+
+    return result;
+}
+
+main();
